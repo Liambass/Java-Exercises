c9117b802a7e40a2d0ab5eb7df1a12390a666b09
Liambass <Liam.Purkiss.spam@gmail.com>
2021-09-15 19:03:53 +0100

    added solution to LargestPrimeFactor iteration exercise

---
+++ IterationExercises/src/com/liambass/iterationexercises/GreatestPrimeFactor.java
@@ -0,0 +1,27 @@
+//Write a method that calculates the largest prime factor of a given number
+
+package com.liambass.iterationexercises;
+
+public class GreatestPrimeFactor {
+
+	public static void main(String[] args) {
+		int num = 455;
+		System.out.println(grPriFact(num));  
+
+	}
+
+	private static int grPriFact(int num) {
+		for(int i = num; i > 1; i--) {
+			if(num % i == 0 && prime(i)) {return i;}
+		}
+		return 1;
+	}
+
+	private static boolean prime(int i) {
+		for (int j = i - 1; j > 1; j--) {
+			if (i % j == 0) {return false;}
+		}
+		return true;
+	}
+
+}
