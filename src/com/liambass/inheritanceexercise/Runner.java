7d4fa77c9aea3f76e3373d79922836203796011a
Liambass <Liam.Purkiss.spam@gmail.com>
2021-09-17 10:35:01 +0100

    experimented with polymophism is inheritanceexercise

--- src/com/liambass/inheritanceexercise/Runner.java
+++ src/com/liambass/inheritanceexercise/Runner.java
@@ -8,25 +8,35 @@
 //- Use encapsulation - private class variables with getters & setters.
 //- Use a separate runner class with a main method.
 
+//- Read through community module on Polymorphism
+//- Have a go at testing polymorphism on your classes from yesterday.
+//- E.g. Create 3 instances of Farmer, stored as Object, Person and Farmer.
+//- Experiment with method access and casting to access methods.
+
 package com.liambass.inheritanceexercise;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		Person bob = new Person("Bob", 43);
-		Farmer giles = new Farmer("Giles", 55, "Cow", "Corn");
 		Banker rupert = new Banker("Rupert", 25, 95452155);
+		
+		Farmer giles = new Farmer("Giles", 55, "Cow", "Corn");
+		Person mcDonald = new Farmer("McDonald", 52, "Alpaca", "Wheat");
+		Object wayne = new Farmer("Wayne", 30, "Dog", "Apples");
 		
 		rupert.setWealth(99152567);
 		System.out.println(bob.sleep()); 
 		System.out.println(giles.sleep());
-		System.out.println(giles.eggs(15));
 		
 		System.out.println(bob);
 		System.out.println(giles);
 		System.out.println(rupert);
 		
-
+		System.out.println(giles.eggs(15));
+		System.out.println(((Farmer)mcDonald).eggs(15));
+		System.out.println(((Person)wayne).sleep());
+		
 	}
 
 }
